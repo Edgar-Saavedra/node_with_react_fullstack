a64faa665e2c82241d199b80d60d1b928674a26f
Edgar Saavedra <edgarsaavedraa@gmail.com>
2021-05-16 22:29:31 -0700

    refactor

--- index.js
+++ index.js
@@ -2,30 +2,13 @@
 // common js modules
 //  a system for requiring files
 const express = require('express');
-const passport = require('passport');
-require('./authentication');
+require('./services/passport');
 
 // this an ES15 modules
 // import express from 'express';
 // will listen for requests
 const app = express();
-
-// internally google strategy, will associate 'google'
-// with the instance of google strategy
-app.get('/auth/google', passport.authenticate('google', {
-  // specifies what access we want to have in the user's
-  // profile and also their email
-  // note: should give example see google docs for scopes
-    scope: ['profile', 'email']
-  })
-);
-
-// we are going to exchange code for user profile
-app.get('/auth/google/callback', passport.authenticate(
-    //use google strategy to handle request
-    'google',
-  )
-)
+require('./routes/authRoutes')(app);
 
 // https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?
 // response_type=code
